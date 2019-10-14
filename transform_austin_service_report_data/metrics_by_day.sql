d43afd2431c527580568ccdc0f38925c78eded86
Alex Lue <a.lue820@gmail.com>
2019-10-14 09:23:08 -0700

    cleaned sql join commands

--- transform_austin_service_report_data/metrics_by_day.sql
+++ transform_austin_service_report_data/metrics_by_day.sql
@@ -2,13 +2,15 @@ CREATE VIEW airflow.metrics_by_day AS
 SELECT COALESCE(d1.date,d2.date) AS date, 
        COALESCE(d1.num_opened,0) AS num_opened, 
        COALESCE(d2.num_closed,0) AS num_closed, 
-       COALESCE(d3.average_num_days_open,0) AS average_num_days_open,
+       COALESCE(d1.average_num_days_open,0) AS average_num_days_open,
        COALESCE(CAST((d4.num_unopened*100.00)/d1.num_opened AS DECIMAL(12, 2)),0) AS percent_unopened,
        COALESCE(CAST((d5.num_never_closed*100.00)/d1.num_opened AS DECIMAL(12, 2)),0) AS percent_never_closed
 FROM
     -- collects number of reports created each day
+    -- & collects average days to close each report
     (SELECT CAST(created_date AS DATE) AS date,
-            COUNT(*) AS num_opened
+            COUNT(*) AS num_opened,
+            CAST(AVG(EXTRACT(DAY FROM close_date-created_date)) AS DECIMAL (12,2)) AS average_num_days_open
     FROM airflow.austin_service_reports
     GROUP BY date) d1
 FULL JOIN
@@ -19,13 +21,6 @@ FULL JOIN
     WHERE close_date IS NOT NULL
     GROUP BY date) d2
 ON (d1.date=d2.date)
-FULL JOIN
-    -- collects average days to close each report
-    (SELECT CAST(created_date AS DATE) AS date, 
-            CAST(AVG(EXTRACT(DAY FROM close_date-created_date)) AS DECIMAL (12,2)) AS average_num_days_open
-    FROM airflow.austin_service_reports
-    GROUP BY date) d3
-ON (d1.date=d3.date)
 FULL JOIN
     -- collects % of reports never opened that were created on each day
     (SELECT CAST(created_date AS DATE) AS date,
