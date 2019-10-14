e81ddf939b217803f8b7776a8e0e6ce23438f020
Alex Lue <a.lue820@gmail.com>
2019-10-14 13:48:45 -0700

    removed individual queries

--- transform_austin_service_report_data/metrics_by_department.sql
+++ transform_austin_service_report_data/metrics_by_department.sql
@@ -37,32 +37,3 @@ FULL JOIN
      GROUP BY department) d4
 ON (d1.department=d4.department)
 ORDER BY d1.department;
-
-
---  total reports by department & avg completion time
-SELECT owning_department AS department,
-       COUNT(*) AS total_reports,
-       CAST(AVG(EXTRACT(DAY FROM close_date-created_date)) AS DECIMAL (12,2)) AS average_num_days_open
-FROM airflow.austin_service_reports
-GROUP BY department;
-
--- total reports still active 
-SELECT owning_department AS department,
-       COUNT(*) AS reports_open
-FROM airflow.austin_service_reports
-WHERE close_date IS NULL
-GROUP BY department;
-
--- total reports closed
-SELECT owning_department AS department,
-       COUNT(*) AS reports_closed
-FROM airflow.austin_service_reports
-WHERE close_date IS NOT NULL
-GROUP BY department;
-
--- total reports never opened
-SELECT owning_department AS department,
-       COUNT(*) AS reports_never_opened
-FROM airflow.austin_service_reports
-WHERE created_date=last_update_date AND close_date is NULL
-GROUP BY department
