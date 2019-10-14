a97800a05a2b1f01a32307422f37c2f579ac9b2f
Alex Lue <a.lue820@gmail.com>
2019-10-14 14:44:03 -0700

    renamed column values

--- transform_austin_service_report_data/metrics_by_day.sql
+++ transform_austin_service_report_data/metrics_by_day.sql
@@ -1,21 +1,21 @@
 CREATE VIEW airflow.metrics_by_day AS
-SELECT COALESCE(d1.date, NULL) AS date,
-       COALESCE(d1.num_reports,0) as num_reports,
-       COALESCE(d3.num_active,0) AS num_active, 
-       COALESCE(d2.num_closed,0) AS num_closed,
-       COALESCE(d4.num_unopened,0) as num_unopened,
+SELECT d1.date,
+       d1.total_reports,
+       COALESCE(d3.reports_active,0) AS reports_active, 
+       COALESCE(d2.reports_closed,0) AS reports_closed,
+       COALESCE(d4.reports_never_opened,0) as reports_never_opened,
        COALESCE(d1.average_num_days_open,0) AS average_num_days_open
 FROM
     -- collects number of reports closed for reports made for each day
     (SELECT CAST(created_date AS DATE) AS date,
-            COUNT(*) AS num_reports,
+            COUNT(*) AS total_reports,
             CAST(AVG(EXTRACT(DAY FROM close_date-created_date)) AS DECIMAL (12,2)) AS average_num_days_open
     FROM airflow.austin_service_reports
     GROUP BY date) d1
 FULL JOIN
     -- collects number of reports closed for reports made for each day
     (SELECT CAST(created_date AS DATE) AS date,
-            COUNT(*) AS num_closed
+            COUNT(*) AS reports_closed
     FROM airflow.austin_service_reports
     WHERE close_date IS NOT NULL
     GROUP BY date) d2
@@ -24,7 +24,7 @@ FULL JOIN
     -- collects number of ongoing reports from each day
     -- can be used to calculate % never closed
     (SELECT CAST(created_date AS DATE) AS date,
-            COUNT(*) AS num_active
+            COUNT(*) AS reports_active
     FROM airflow.austin_service_reports
     WHERE close_date is NULL
     GROUP BY date) d3
@@ -32,7 +32,7 @@ ON (d1.date=d3.date)
 FULL JOIN
     -- collects # of reports never opened that were created on each day
     (SELECT CAST(created_date AS DATE) AS date,
-            COUNT(*) AS num_unopened
+            COUNT(*) AS reports_never_opened
      FROM airflow.austin_service_reports
      WHERE created_date=last_update_date AND close_date is NULL
      GROUP BY date) d4
--- transform_austin_service_report_data/metrics_by_department.sql
+++ transform_austin_service_report_data/metrics_by_department.sql
@@ -1,7 +1,7 @@
--- CREATE VIEW airflow.metrics_by_department AS
+CREATE VIEW airflow.metrics_by_department AS
 SELECT d1.department, 
        d1.total_reports AS total_reports,
-       COALESCE(d2.reports_open,0) AS reports_active,
+       COALESCE(d2.reports_active,0) AS reports_active,
        COALESCE(d3.reports_closed,0) AS reports_closed,
        COALESCE(d4.reports_never_opened,0) AS reports_never_opened,
        COALESCE(d1.average_num_days_open,0) AS average_num_days_open
@@ -15,7 +15,7 @@ FROM
 FULL JOIN
     -- total reports still opened 
     (SELECT owning_department AS department,
-            COUNT(*) AS reports_open
+            COUNT(*) AS reports_active
      FROM airflow.austin_service_reports
      WHERE close_date IS NULL
      GROUP BY department) d2
